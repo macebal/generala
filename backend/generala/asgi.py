0d21b8a882b74112d78b9f680e189bacafa437c5
Mariano Acebal <macebal@outlook.com.ar>
2025-04-27 12:09:34 -0300

    chore: configured daphne asgi server

--- backend/generala/asgi.py
+++ backend/generala/asgi.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'generala.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "generala.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+    }
+)
