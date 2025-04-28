7e2fb9439fa5bd2a53f47badcc4288fb5baf7119
Mariano Acebal <macebal@outlook.com.ar>
2025-04-27 22:38:39 -0300

    feat: added `player_id` and ability to finish player turn

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -1,13 +1,28 @@
 import json
 import random
-
+from urllib.parse import parse_qs
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class GameConsumer(AsyncWebsocketConsumer):
+    player_ids = list()
+    current_player_index = 0
+
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"game_{self.room_name}"
+
+        query_params = parse_qs(self.scope["query_string"].decode())
+        player_id = query_params.get("player_id", [None])[0]
+
+        if player_id is None:
+            # Do not accept connections that don't contain player id
+            self.close()
+
+        self.player_id = player_id
+
+        if player_id not in self.player_ids:
+            self.player_ids.append(player_id)
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
@@ -16,12 +31,13 @@ class GameConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
         # Leave room group
+        self.player_ids.remove(self.player_id)
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         event = json.loads(text_data)
-
+        print(f"{self.player_ids}")
         match event.get("type"):
             case "chat.message":
                 if (
@@ -30,13 +46,26 @@ class GameConsumer(AsyncWebsocketConsumer):
                 ):
                     return
             case "dice.roll":
+                print(f"Dice rolled by {self.player_id}")
                 number_of_dice = event.get("content", {}).get("number_of_dice", 0)
-                results = self.roll_dice(number_of_dice)
-                event = {"type": "dice.roll.result", "content": {"result": results}}
+                event = {
+                    "type": "dice.roll.result",
+                    "content": {
+                        "result": self.roll_dice(number_of_dice),
+                    },
+                }
+            case "player.turn.finish":
+                print(
+                    f"Player {self.player_ids[self.current_player_index]} finished its turn"
+                )
+                next_player_id = self.finish_turn()
+                event = {
+                    "type": "player.turn.next",
+                    "content": {"player_id": next_player_id},
+                }
             case _:
                 print(f"invalid type: {event.get('type')}")
                 return
-        print(f"{event=}")
 
         await self.channel_layer.group_send(self.room_group_name, event)
 
@@ -51,9 +80,23 @@ class GameConsumer(AsyncWebsocketConsumer):
         # event = {'type': 'dice.roll.result', 'content': {'result': list[str]}}
         await self.send(text_data=json.dumps(event))
 
+    async def player_turn_next(self, event):
+        # corresponds to the `player.next.turn` type
+        # event = {'type': 'player.next.turn', 'content': {'next_player': str}}
+        await self.send(text_data=json.dumps(event))
+
     @staticmethod
     def roll_dice(number_of_dice: int) -> list[int]:
         if number_of_dice == 0:
             return []
 
         return [random.randint(1, 6) for _ in range(number_of_dice)]
+
+    def finish_turn(self) -> str:
+        """Finished the current player id turn and moves the turn to the next.
+        Returns the player id whose turn is next.
+        """
+        self.current_player_index = (self.current_player_index + 1) % len(
+            self.player_ids
+        )
+        return self.player_ids[self.current_player_index]
