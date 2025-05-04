36b2159521e1c2e11a71763622444a7212906f09
Mariano Acebal <macebal@outlook.com.ar>
2025-05-04 17:07:26 -0300

    feat: update GameConsumer to manage player state and improve event handling

--- backend/game/consumers.py
+++ backend/game/consumers.py
@@ -1,12 +1,14 @@
 import json
 import random
 from urllib.parse import parse_qs
+from game.generala import GameState
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class GameConsumer(AsyncWebsocketConsumer):
     player_ids = list()
     current_player_index = 0
+    game_state = GameState()
 
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
@@ -21,76 +23,83 @@ class GameConsumer(AsyncWebsocketConsumer):
 
         self.player_id = player_id
 
-        if player_id not in self.player_ids:
-            self.player_ids.append(player_id)
+        self.game_state.add_player(player_id)
 
         # Join room group
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
         await self.accept()
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "player.status.joined",
-                "content": {
-                    "player_id": self.player_id,
-                    "all_players": self.player_ids,
-                },
-            },
-        )
+        await self.handle_player_joined()
 
     async def disconnect(self, close_code):
         # Leave room group
-        self.player_ids.remove(self.player_id)
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                "type": "player.status.left",
-                "content": {
-                    "player_id": self.player_id,
-                    "all_players": self.player_ids,
-                },
-            },
-        )
+        await self.handle_player_left()
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         event = json.loads(text_data)
-        print(f"{self.player_ids}")
-        match event.get("type"):
-            case "chat.message":
-                if (
-                    not event.get("content", {}).get("message")
-                    or len(event["content"]["message"]) == 0
-                ):
-                    return
-            case "dice.roll":
-                print(f"Dice rolled by {self.player_id}")
-                number_of_dice = event.get("content", {}).get("number_of_dice", 0)
-                event = {
-                    "type": "dice.roll.result",
-                    "content": {
-                        "result": self.roll_dice(number_of_dice),
-                    },
-                }
-            case "player.turn.finish":
-                print(
-                    f"Player {self.player_ids[self.current_player_index]} finished its turn"
-                )
-                next_player_id = self.finish_turn()
-                event = {
-                    "type": "player.turn.next",
-                    "content": {"player_id": next_player_id},
-                }
-            case _:
-                print(f"invalid type: {event.get('type')}")
-                return
+
+        handlers = {
+            "chat.message": self.handle_chat_message,
+            "dice.roll": self.handle_dice_roll,
+            "player.turn.finish": self.handle_player_turn_finish,
+            "player.status.joined": self.handle_player_joined,
+        }
+
+        try:
+            handler = handlers[event.get("type")]
+        except Exception as e:
+            print(f"invalid type {event.get('type')}: {e}")
+            return
+
+        await handler(event)
+
+    async def handle_chat_message(self, event):
+        if (
+            not event.get("content", {}).get("message")
+            or len(event["content"]["message"]) == 0
+        ):
+            return
+        await self.channel_layer.group_send(self.room_group_name, event)
+
+    async def handle_dice_roll(self, event):
+        print(f"Dice rolled by {self.player_id}")
+        number_of_dice = event.get("content", {}).get("number_of_dice", 0)
+        event = {
+            "type": "dice.roll.result",
+            "content": {
+                "result": self.roll_dice(number_of_dice),
+            },
+        }
 
         await self.channel_layer.group_send(self.room_group_name, event)
 
-    # Receive message from room group
+    async def handle_player_turn_finish(self, event):
+        print(f"Player {self.player_ids[self.current_player_index]} finished its turn")
+        next_player_id = self.finish_turn()
+        event = {
+            "type": "player.turn.next",
+            "content": {"player_id": next_player_id},
+        }
+
+        await self.channel_layer.group_send(self.room_group_name, event)
+
+    async def handle_player_joined(self):
+        print("whaa")
+        self.game_state.add_player(self.player_id)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {"type": "game.status", "content": self.game_state.model_dump_json()},
+        )
+
+    async def handle_player_left(self):
+        self.game_state.remove_player(self.player_id)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {"type": "game.status", "content": self.game_state.model_dump_json()},
+        )
+
     async def chat_message(self, event):
         # corresponds to the `chat.message` type
         # event = {'type': 'chat.message', 'content' : {'message': str}}
@@ -106,15 +115,20 @@ class GameConsumer(AsyncWebsocketConsumer):
         # event = {'type': 'player.next.turn', 'content': {'next_player': str}}
         await self.send(text_data=json.dumps(event))
 
-    async def player_status_joined(self, event):
-        # corresponds to the `player.status.joined` type
-        # event = {'type': 'player.status.joined', 'content': {'player_id': str, 'all_players': list[str]}}
+    async def game_status(self, event):
+        # corresponds to the `player.next.turn` type
+        # event = {'type': 'player.next.turn', 'content': {...}}
         await self.send(text_data=json.dumps(event))
 
-    async def player_status_left(self, event):
-        # corresponds to the `player.status.joined` type
-        # event = {'type': 'player.status.left', 'content': {'player_id': str, 'all_players': list[str]}}
-        await self.send(text_data=json.dumps(event))
+    # async def player_status_joined(self, event):
+    #     # corresponds to the `player.status.joined` type
+    #     # event = {'type': 'player.status.joined', 'content': {'player_id': str, 'all_players': list[str]}}
+    #     await self.send(text_data=json.dumps(event))
+
+    # async def player_status_left(self, event):
+    #     # corresponds to the `player.status.joined` type
+    #     # event = {'type': 'player.status.left', 'content': {'player_id': str, 'all_players': list[str]}}
+    #     await self.send(text_data=json.dumps(event))
 
     @staticmethod
     def roll_dice(number_of_dice: int) -> list[int]:
